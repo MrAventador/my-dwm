14161291e362c3cde9825909f7b120e56ba3c6fd
Omid <147955568+MrAventador@users.noreply.github.com>
2023-12-01 07:56:50 +0330

    Set up volume keys

--- config.def.h
+++ config.def.h
@@ -13,6 +13,11 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+
+static const char *up_vol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",   NULL };
+static const char *down_vol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",   NULL };
+static const char *mute_vol[] = { "pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle", NULL };
+
 static const char *fonts[]          = { "monospace:size=16" };
 static const char dmenufont[]       = "monospace:size=16";
 static const char col_gray1[]       = "#222222";
@@ -111,6 +116,9 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ 0,         XK_Print, spawn, SHCMD("/home/omid/scripts/screenshot.sh") },
         { ShiftMask, XK_Print, spawn, SHCMD("/home/omid/scripts/screenshotsel.sh") },
+	{ 0, XF86XK_AudioMute,        spawn, {.v = mute_vol } },
+       	{ 0, XF86XK_AudioLowerVolume, spawn, {.v = down_vol } },
+       	{ 0, XF86XK_AudioRaiseVolume, spawn, {.v = up_vol } },
 };
 
 /* button definitions */
